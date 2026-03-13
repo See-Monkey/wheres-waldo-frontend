8ebb24847edbc181762db9a2342acdb60ad3c7a6
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-12 19:43:58 -0600

    Setup project

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# React + Vite
+# The Odin Project - Where's Waldo - React Frontend
 
-Title
+- React
+- Vite
+- React Router
+- Vitest
+- CSS Modules
+
+Backend project available at https://github.com/See-Monkey/wheres-waldo-backend
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,19 +1,15 @@
 import { createBrowserRouter } from "react-router";
-import App from "./App.jsx";
+import App from "../App.jsx";
 
-import Home from "./pages/Home/Home.jsx";
-import Login from "./pages/Login/Login.jsx";
-import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import Home from "../pages/Home/Home.jsx";
+import ErrorPage from "../pages/ErrorPage/ErrorPage.jsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
 		errorElement: <ErrorPage />,
-		children: [
-			{ index: true, element: <Home /> },
-			{ path: "login", element: <Login /> },
-		],
+		children: [{ index: true, element: <Home /> }],
 	},
 ]);
 
