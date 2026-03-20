805a7c44ec986799268b855edde3389e192b1ed3
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-19 21:38:04 -0600

    Add character sprites to HUD and style

--- src/components/HUD/HUD.jsx
+++ src/components/HUD/HUD.jsx
@@ -1,4 +1,9 @@
 import styles from "./HUD.module.css";
+import waldoSprite from "../../images/waldo.png";
+import wendaSprite from "../../images/wenda.png";
+import wizardSprite from "../../images/wizard-whitebeard.png";
+import odlawSprite from "../../images/odlaw.png";
+import xOverlay from "../../images/x-overlay.png";
 
 export default function HUD({
 	time,
@@ -7,18 +12,41 @@ export default function HUD({
 	feedback,
 	completionTime,
 }) {
+	const spriteMap = {
+		waldo: waldoSprite,
+		wenda: wendaSprite,
+		wizard: wizardSprite,
+		odlaw: odlawSprite,
+	};
+
 	return (
 		<div className={styles.hud}>
-			<div className={styles.timer}>
-				{completionTime !== null
-					? `Completed in ${completionTime}s`
-					: `Time: ${time}s`}
-			</div>
-			<div className={styles.characters}>
-				Found: {foundCharacters.length} / {characters.length}
-			</div>
-			<div className={`${styles.feedback} ${!feedback ? styles.hidden : ""}`}>
-				{feedback || "\u00A0"}
+			<div className={styles.hudContainer}>
+				<div className={styles.timer}>
+					{completionTime !== null
+						? `Completed in ${completionTime}s`
+						: `Time: ${time}s`}
+				</div>
+				<div className={`${styles.feedback} ${!feedback ? styles.hidden : ""}`}>
+					{feedback || "\u00A0"}
+				</div>
+				<div className={styles.characterContainer}>
+					{characters.map((char) => {
+						const isFound = foundCharacters.includes(char);
+						return (
+							<div key={char} className={styles.characterWrapper}>
+								<img
+									src={spriteMap[char]}
+									alt={char}
+									className={styles.characterSprite}
+								/>
+								{isFound && (
+									<img src={xOverlay} alt="found" className={styles.overlay} />
+								)}
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
