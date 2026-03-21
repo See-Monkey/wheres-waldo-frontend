7fdc0e8d3162fa2eb3a84347809daa4559c8e97d
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-21 17:04:56 -0600

    Complete ErrorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,22 @@
+import styles from "./ErrorPage.module.css";
+import confusedWaldo from "../../images/confused-waldo.png";
+import { Link } from "react-router";
+
 export default function ErrorPage() {
 	return (
-		<main>
-			<h1>Error Page</h1>
+		<main className={styles.main}>
+			<div className={styles.errorContainer}>
+				<h1>Hmm, something isn't right</h1>
+				<div className={styles.errorContent}>
+					<img src={confusedWaldo} alt="confused monkey" />
+					<p>
+						The page you're looking for doesn't seem to exist or has been moved.
+					</p>
+				</div>
+				<p>
+					Click <Link to={"/"}>here</Link> to go back to safety.
+				</p>
+			</div>
 		</main>
 	);
 }
